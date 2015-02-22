7b4f0c90da5fc7351f2b5809868b7d967991c1e8
ekoraytascilar <ekoraytascilar@yahoo.com>
2015-02-23 00:28:04 +0200

    Update cachematrix.R
    Here's my assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function creates a square matrix from a single integer by sampling random normal numbers
+## and creates functions that are able to get the assigned variables from the very 
+## environment in which they were created.
 
-## Write a short comment describing this function
+## A small difference from the original; my makeCacheMatrix function creates a random matrix from an integer 
 
-makeCacheMatrix <- function(x = matrix()) {
+## x with a dimention of (x,x) that includes x^2 elements drawn from a random distribution with a mean of x and 
+## sd of approximately x/2. if you don't pass a non-zero positive integer as an argument it will throw an error.
 
+makeCacheMatrix <- function(x = numeric()) {
+  if (as.integer(x)!=x|as.integer(x)<=0){ 
+    print ("Not a nice number, need a non-zero positive integer")
+    stop
+  }
+  else n<-matrix(as.integer(rnorm(x^2, mean=x, sd=as.integer(x/2))), c(x,x))
+  
+  m <- NULL
+  set <- function(y) {
+    n <<- y
+    m <<- NULL
+  }
+  get <- function() n
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Operation of this neccesitates that output of the makeCacheMatrix function is passed on a vector.
+## when you pass this result vector as the argument of cacheSolve, it in the first execution caches the inverse of the 
+## matrix created in the previous function and in subsequent re-executions verifies that there's a cached
+## inverse matrix value and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached solve")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
